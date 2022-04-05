5240757f17b807cf0d343ead8fe4bd7a19b81f6d
kairemerson <kairemerson@gmail.com>
2022-04-04 22:47:50 -0300

    criado mock do componente home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,21 +1,42 @@
 import React from "react";
-import { Container } from "./styles";
+import { Container, MovieList,Movie } from "./styles";
 
 function Home(){
+    const movies = [
+        {
+            id: 1,
+            title: 'spider man',
+            image_url: 'https://image.api.playstation.com/vulcan/img/rnd/202011/0714/vuF88yWPSnDfmFJVTyNJpVwW.png'
+        },
+        {
+            id: 2,
+            title: 'batman',
+            image_url: 'https://image.api.playstation.com/vulcan/img/rnd/202011/0714/vuF88yWPSnDfmFJVTyNJpVwW.png'
+        },
+        {
+            id: 3,
+            title: 'avengers',
+            image_url: 'https://image.api.playstation.com/vulcan/img/rnd/202011/0714/vuF88yWPSnDfmFJVTyNJpVwW.png'
+        }
+    ]
     return(
         <Container>
             <h1>Movies</h1>
-            <ul>
-                <li>
-                    <img src="" alt=""/>
-                </li>
-                <li>
-
-                </li>
-                <li>
-
-                </li>
-            </ul>
+            <MovieList>
+                {movies.map(movie =>{
+                    return(
+                    <Movie key={movie.id}>
+                        <a href="http://google.com">
+                            <img src={movie.image_url} alt={movie.title}/>
+                        </a>
+                        <span>{movie.title}</span>
+                    </Movie>
+                    )
+                })}
+                
+                
+                
+            </MovieList>
         </Container>
     )
 }
--- src/components/Home/styles.js
+++ src/components/Home/styles.js
@@ -6,8 +6,35 @@ export const Container = styled.div`
         margin: 4rem 0;
     }
 
-    ul{
-        list-style: none;
-        
+    
+`
+
+export const MovieList = styled.ul`
+    list-style: none;
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    
+    
+    
+`
+
+export const Movie = styled.li`
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    img{
+        max-width: 220px;
+        border-radius: 1rem;
+    }
+    span{
+        font-size: 1.4rem;
+        margin-top: 1rem;
+    }
+    a{
+        transition: 0.3s;
+    }
+    a:hover{
+        transform: scale(1.1);
     }
 `
