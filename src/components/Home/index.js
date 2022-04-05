b8c021a672b8dbe3db6ca7f4fb4e42f093e1fde6
Remerson Monteiro <kairemerson@gmail.com>
2022-04-04 21:18:46 -0300

    projeto inicializado

---
+++ src/components/Home/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Container } from "./styles";
+
+function Home(){
+    return(
+        <Container>
+            <h1>Movies</h1>
+            <ul>
+                <li>
+                    <img src="" alt=""/>
+                </li>
+                <li>
+
+                </li>
+                <li>
+
+                </li>
+            </ul>
+        </Container>
+    )
+}
+
+export default Home
